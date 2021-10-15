f69888973d4fcf7118c259b56a8f85be26a47803
ruro122020 <ruro122020@gmail.com>
2021-10-15 13:59:54 -0600

    StreamList component setup complete

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from "../apis/streams";
+import history from "../history";
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -22,10 +23,14 @@ export const signOut = () => {
   };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-  const response = await streams.post("/streams", formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const response = await streams.post("/streams", { ...formValues, userId });
 
   dispatch({ type: CREATE_STREAM, payload: response.data });
+
+  //programatic navigation to get user back to the root route
+  history.push("/");
 };
 
 export const fetchStreams = () => async (dispatch) => {
---
+++ src/history.js
@@ -0,0 +1,6 @@
+//created a history object we can maintain so we can use it in action creators
+
+//history was installed as a dependency automatically when react-router-dom was installed
+import { createBrowserHistory } from "history";
+
+export default createBrowserHistory();
